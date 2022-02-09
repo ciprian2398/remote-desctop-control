ad5b1e099867c902498ff053df7315770c83fa75
Nicuta <ciprian.nicuta@elefant.ro>
2022-02-09 17:52:06 +0200

    fix pom and image

--- src/main/java/StreamDemo.java
+++ src/main/java/StreamDemo.java
@@ -6,39 +6,61 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class StreamDemo {
     ImageProvider imageProvider = new ImageProvider();
 
-    public void init(){
+    public void init() {
         HttpServer server = null;
         try {
             server = HttpServer.create(new InetSocketAddress(8080), 0);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        server.createContext("/video", new MyHandler());
+        server.createContext("/video", new FrameHandler());
+        server.createContext("/", new MainHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
-    class MyHandler implements HttpHandler {
+    class FrameHandler implements HttpHandler {
         @Override
-        public void handle(HttpExchange t) throws IOException {
+        public void handle(HttpExchange exchange) throws IOException {
             BufferedImage bi = imageProvider.getImg();
 
             ByteArrayOutputStream tmp = new ByteArrayOutputStream();
             ImageIO.write(bi, "png", tmp);
-            tmp.close();
 
-            t.sendResponseHeaders(200, tmp.size());
-            OutputStream os = t.getResponseBody();
+            exchange.sendResponseHeaders(200, tmp.size());
+            OutputStream os = exchange.getResponseBody();
             os.write(tmp.toByteArray());
             os.close();
-            System.out.println("zapros");
         }
     }
 
+    class MainHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            try (
+                    InputStream in = getClass().getClassLoader().getResourceAsStream("image.html");
+                    OutputStream out = exchange.getResponseBody();
+            ) {
+                int length = 0;
+                byte[] bytes = new byte[1024];
+
+                exchange.sendResponseHeaders(200, in.available());
+
+                // copy data from input stream to output stream
+                while ((length = in.read(bytes)) != -1) {
+                    out.write(bytes, 0, length);
+                }
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
